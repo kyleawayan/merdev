6834693dfb5adc520f157a84d36ba8d8c8ba3b75
Kyle Awayan <56655579+kyleawayan@users.noreply.github.com>
2021-04-07 19:12:47 -0700

    fix text being behind image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "../styles/Landing.module.css";
-import Image from 'next/image'
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div>
-
       {/* <div className = "container">
         <div className="text">i am text</div>
         <div className = "new">
@@ -12,13 +12,10 @@ export default function Home() {
         </div>
       </div> */}
 
-      <div className = {styles.Landing}>
-        <Image src="/aerial2.png" alt="me" layout = "fill"/>
-        <div className = {styles.text}>test text</div>
+      <div className={styles.Landing}>
+        <Image src="/aerial2.png" alt="me" layout="fill" objectFit="cover" />
+        <div className={styles.text}>test text</div>
       </div>
-
     </div>
   );
 }
-
-
