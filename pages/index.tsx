6f1cca96341be91eda5c752f9225caaf2a055f37
gurubac <gbrbachchu@gmail.com>
2021-04-07 18:56:35 -0700

    added image to index figuring out how to put text above it

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,24 @@
 import React from "react";
-
+import styles from "../styles/Landing.module.css";
+import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <div className="container">i am text</div>
+
+      {/* <div className = "container">
+        <div className="text">i am text</div>
+        <div className = "new">
+          <Image src="/aerial2.png" alt="me" layout = "fill" className = "aerial"/>
+        </div>
+      </div> */}
+
+      <div className = {styles.Landing}>
+        <Image src="/aerial2.png" alt="me" layout = "fill"/>
+        <div className = {styles.text}>test text</div>
+      </div>
+
     </div>
   );
 }
+
+
