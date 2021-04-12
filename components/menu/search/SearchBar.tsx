9fcef1682fc17e35f732715f6df09db827d3e071
Kyle Awayan <56655579+kyleawayan@users.noreply.github.com>
2021-04-11 23:26:03 -0700

    improve question mobile styles

--- components/menu/search/SearchBar.tsx
+++ components/menu/search/SearchBar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "../../styles/menu/SearchBar.module.css";
+import styles from "../../../styles/menu/SearchBar.module.css";
 
 export default function SearchBar() {
   return (
