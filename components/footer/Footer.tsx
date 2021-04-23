666ae607dea578c0804f6417f580c23004e90a0d
Kyle Awayan <56655579+kyleawayan@users.noreply.github.com>
2021-04-22 22:24:21 -0700

    improve recent questions sidebar style, fix some footer styles

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -9,21 +9,23 @@ export default function Footer() {
 
   return (
     <div className={styles.footerParent}>
-      <div className={styles.footerChild}>
-        <Logo />
-        <div className={styles.version}>v0.3.0</div>
-      </div>
-      <div className={styles.footerChild}>
-        <div className={styles.menuButtons}>
-          <a
-            href="https://github.com/kyleawayan/merdev/issues/new"
-            target="_blank"
-          >
-            <div className={styles.notMenuButton}>Report an issue</div>
-          </a>
-          <a href="https://github.com/kyleawayan/merdev" target="_blank">
-            <div className={styles.notMenuButton}>GitHub</div>
-          </a>
+      <div className={styles.container}>
+        <div className={styles.footerChild}>
+          <Logo />
+          <div className={styles.version}>v0.3.0</div>
+        </div>
+        <div className={styles.footerChild}>
+          <div className={styles.menuButtons}>
+            <a
+              href="https://github.com/kyleawayan/merdev/issues/new"
+              target="_blank"
+            >
+              <div className={styles.notMenuButton}>Report an issue</div>
+            </a>
+            <a href="https://github.com/kyleawayan/merdev" target="_blank">
+              <div className={styles.notMenuButton}>GitHub</div>
+            </a>
+          </div>
         </div>
       </div>
     </div>
--- components/questionViewer/answers/AddAnswer.tsx
+++ components/questionViewer/answers/AddAnswer.tsx
@@ -9,7 +9,12 @@ export default function AddAnswer({ questionId }: AddAnswerProps) {
   const [editorOpen, setEditorOpen] = useState(false);
   return (
     <div>
-      <button onClick={() => setEditorOpen(!editorOpen)}>post an answer</button>
+      <button
+        onClick={() => setEditorOpen(!editorOpen)}
+        style={{ marginBottom: "20px" }}
+      >
+        Post an answer
+      </button>
       {editorOpen && (
         <YourAnswer
           questionId={questionId}
