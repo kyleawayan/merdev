dbd284fc547cbea54b426eba02767fea26fe6adf
Kyle Awayan <56655579+kyleawayan@users.noreply.github.com>
2021-04-23 19:38:23 -0700

    v0.4.0

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
       <div className={styles.container}>
         <div className={styles.footerChild}>
           <Logo />
-          <div className={styles.version}>v0.3.1</div>
+          <div className={styles.version}>v0.4.0</div>
         </div>
         <div className={styles.footerChild}>
           <div className={styles.menuButtons}>
